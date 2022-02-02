f87424fa565589b19ea733b777d1dce6375c79ae
Pofay <gianCarlo.gilos@gmail.com>
2022-02-02 20:50:18 +0800

    Added Initial Navigation component

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { Route, Routes } from 'react-router-dom'
 import AllMeetupsPage from './pages/AllMeetupsPage'
 import NewMeetup from './pages/NewMeetup'
 import Favorites from './pages/Favorites'
+import MainNavigation from './components/layout/MainNavigation'
 
 const App = () => {
   return (
     <div>
+      <MainNavigation />
       <Routes>
         <Route path='/' element={<AllMeetupsPage />} />
         <Route path='/new-meetup' element={<NewMeetup />} />
