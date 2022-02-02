3c3e0675557fee3189d66355a8698f969f2c256a
Pofay <gianCarlo.gilos@gmail.com>
2022-02-02 21:24:07 +0800

    Extracted custom wrapper component Card

--- src/components/meetups/MeetupItem.jsx
+++ src/components/meetups/MeetupItem.jsx
@@ -1,19 +1,22 @@
+import Card from '../ui/Card'
 import classes from './MeetupItem.module.css'
 
 const MeetupItem = ({ key, title, image, address, description, onClick }) => {
   return (
     <li key={key} className={classes.item}>
-      <div className={classes.image}>
-        <img src={image} alt='' />
-      </div>
-      <div className={classes.content}>
-        <h3>{title}</h3>
-        <address>{address}</address>
-        <p>{description}</p>
-      </div>
-      <div className={classes.actions}>
-        <button onClick={onClick}>To Favorites</button>
-      </div>
+      <Card>
+        <div className={classes.image}>
+          <img src={image} alt='' />
+        </div>
+        <div className={classes.content}>
+          <h3>{title}</h3>
+          <address>{address}</address>
+          <p>{description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button onClick={onClick}>To Favorites</button>
+        </div>
+      </Card>
     </li>
   )
 }
