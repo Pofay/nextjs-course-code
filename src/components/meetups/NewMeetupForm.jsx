b5995ae66b878b54f81bd79ef124c9edd13f2f13
Gian Carlo Gilos <gian@isr.com.ph>
2022-03-10 16:07:15 +0800

    Opted to use useState instead of useRef for getting input data from Form

--- src/components/meetups/NewMeetupForm.jsx
+++ src/components/meetups/NewMeetupForm.jsx
@@ -1,28 +1,73 @@
+import { useState } from 'react';
 import Card from '../ui/Card';
 import classes from './NewMeetupForm.module.css';
 
-const NewMeetupForm = () => {
+const NewMeetupForm = ({ postNewMeetup }) => {
+  const [title, setTitle] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [address, setAddress] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleChange = (event, setterFn) => {
+    const value = event.target.value;
+    setterFn(value);
+  };
+
   const submitHandler = (event) => {
     event.preventDefault();
+
+    const payload = {
+      title,
+      imageUrl,
+      address,
+      description,
+    };
+
+    postNewMeetup(payload);
   };
+
   return (
     <Card>
       <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='title'>Title</label>
-          <input type='text' required id='title' />
+          <input
+            type='text'
+            required
+            id='title'
+            value={title}
+            onChange={(event) => handleChange(event, setTitle)}
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor='image'>Image URL</label>
-          <input type='url' required id='image' />
+          <input
+            type='url'
+            required
+            id='image'
+            value={imageUrl}
+            onChange={(event) => handleChange(event, setImageUrl)}
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor='address'>Address</label>
-          <input type='text' required id='address' />
+          <input
+            type='text'
+            required
+            id='address'
+            value={address}
+            onChange={(event) => handleChange(event, setAddress)}
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor='description'>Description</label>
-          <textarea id='description' required rows='5'></textarea>
+          <textarea
+            id='description'
+            required
+            rows='5'
+            value={description}
+            onChange={(event) => handleChange(event, setDescription)}
+          ></textarea>
         </div>
         <div className={classes.actions}>
           <button>Add Meetup</button>
--- src/pages/NewMeetup.jsx
+++ src/pages/NewMeetup.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import NewMeetupForm from '../components/meetups/NewMeetupForm'
+import React from 'react';
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 const NewMeetup = () => {
+  const postNewMeetup = (payload) => {
+    console.log(payload);
+  };
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm postNewMeetup={postNewMeetup} />
     </section>
-  )
-}
+  );
+};
 
-export default NewMeetup
+export default NewMeetup;
