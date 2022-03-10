c580f1cbf12eb8980fcee27ac536c9c74eabdc4a
Gian Carlo Gilos <gian@isr.com.ph>
2022-03-10 15:41:02 +0800

    Added dummy event handler for meetup form

--- src/components/meetups/NewMeetupForm.jsx
+++ src/components/meetups/NewMeetupForm.jsx
@@ -1,10 +1,13 @@
-import Card from '../ui/Card'
-import classes from './NewMeetupForm.module.css'
+import Card from '../ui/Card';
+import classes from './NewMeetupForm.module.css';
 
 const NewMeetupForm = () => {
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
   return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='title'>Title</label>
           <input type='text' required id='title' />
@@ -26,7 +29,7 @@ const NewMeetupForm = () => {
         </div>
       </form>
     </Card>
-  )
-}
+  );
+};
 
-export default NewMeetupForm
+export default NewMeetupForm;
