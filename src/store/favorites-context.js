c692acc9d2e0d6574156fa19978ca315c77626d9
Pofay <gianCarlo.gilos@gmail.com>
2022-03-14 21:35:00 +0800

    Created Favorites context

---
+++ src/store/favorites-context.js
@@ -0,0 +1,41 @@
+import { createContext, useState } from 'react'
+
+const FavoritesContext = createContext({
+  favorites: [],
+  totalFavorites: 0,
+  addFavorite: (meetup) => {},
+  removeFavorite: (meetupId) => {},
+  itemIsFavorite: (meetupId) => {},
+})
+
+const FavoritesContextProvider = (props) => {
+  const [userFavorites, setUserFavorites] = useState([])
+
+  const addFavoriteHandler = (meetup) => {
+    setUserFavorites((prevFavorites) => prevFavorites.concat(meetup))
+  }
+
+  const removeFavoriteHandler = (meetupId) => {
+    setUserFavorites((prevFavorites) =>
+      prevFavorites.filter((meetup) => meetup.id !== meetupId)
+    )
+  }
+
+  const itemIsFavoriteHandler = (meetupId) => {
+    return userFavorites.some((meetup) => meetup.id === meetupId)
+  }
+
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
+    addFavorite: addFavoriteHandler,
+    removeFavorite: removeFavoriteHandler,
+    itemIsFavorite: itemIsFavoriteHandler,
+  }
+
+  return (
+    <FavoritesContext.Provider value={context}>
+      {props.children}
+    </FavoritesContext.Provider>
+  )
+}
