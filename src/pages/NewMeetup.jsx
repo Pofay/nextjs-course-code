e6a082b81e5b200c948b80133072e149b764a49f
Gian Carlo Gilos <gian@isr.com.ph>
2022-03-15 10:43:27 +0800

    Used postNewMeetup in API file

--- src/pages/NewMeetup.jsx
+++ src/pages/NewMeetup.jsx
@@ -1,27 +1,12 @@
 import React from 'react';
+import MeetupAPI from '../api/meetup-api';
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
-const tee = (data) => {
-  console.log(data);
-  return data;
-};
-
 const NewMeetup = () => {
-  const postNewMeetup = (payload) =>
-    fetch('/meetups', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(payload),
-    })
-      .then((res) => res.json())
-      .then((res) => tee(res.data));
-
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm postNewMeetup={postNewMeetup} />
+      <NewMeetupForm postNewMeetup={MeetupAPI.postNewMeetup} />
     </section>
   );
 };
